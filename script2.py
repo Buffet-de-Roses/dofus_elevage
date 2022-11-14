8f1f7826a26889cc781661771fe2634c72b131ec
tmarescot <tom.marescot-ext@kering.com>
2022-11-14 16:35:53 +0100

    sort all csv file for efficiency

--- script2.py
+++ script2.py
@@ -1,15 +1,15 @@
 import csv
 
-mangeoire = open('mangeoire.csv', 'w')
-baffeur = open('baffeur.csv', 'w')
-foudroyeur = open('foudroyeur.csv', 'w')
-caresseur = open('caresseur.csv', 'w')
-dragofesse = open('dragofesse.csv', 'w')
-abreuvoir = open('abreuvoir.csv', 'w')
+mangeoire = open('csv/mangeoire.csv', 'w')
+baffeur = open('csv/baffeur.csv', 'w')
+foudroyeur = open('csv/foudroyeur.csv', 'w')
+caresseur = open('csv/caresseur.csv', 'w')
+dragofesse = open('csv/dragofesse.csv', 'w')
+abreuvoir = open('csv/abreuvoir.csv', 'w')
 
 writer = csv.writer(mangeoire)
 
-with open('do.csv') as csv_file:
+with open('csv/do.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         if 'Mangeoire' in row[0]:
---
+++ script3.py
@@ -0,0 +1,25 @@
+import csv, operator, sys
+
+csv_file = open(sys.argv[1], 'r+')
+
+reader = csv.reader(csv_file, delimiter=',')
+
+sorted_array = []
+for row in reader:
+    if 'efficacité' in row[1]:
+        continue
+    sorted_array.append((row[0], int(row[1]), row[2]))
+
+csv_file.close()
+
+open(sys.argv[1], 'w').close()
+
+csv_file = open(sys.argv[1], 'w')
+writer = csv.writer(csv_file)
+
+sorted_array.sort(key=lambda a:a[1])
+writer.writerow(['name', 'efficacité', 'utilisation'])
+for row in sorted_array:
+    writer.writerow(row)
+
+
